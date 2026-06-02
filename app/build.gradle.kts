e23a8eb21b85ff765b7a7a3d8f8691a673763662
Kor1 <kingzcheung@gmail.com>
2026-06-02 21:48:21 +0800

    chore(app): 更新版本号到2.3.0-beta4
    更新应用版本号从2.3.0-beta3 (versionCode 24) 到2.3.0-beta4 (versionCode 25)
    
    fix(librime): 修复子项目提交状态标记
    
    更新librime子项目的提交状态，标记为dirty状态以反映本地修改

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -308,8 +308,8 @@ android {
         applicationId = "com.kingzcheung.xime"
         minSdk = 28
         targetSdk = 35
-        versionCode = 24
-        versionName = "2.3.0-beta3"
+        versionCode = 25
+        versionName = "2.3.0-beta4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
