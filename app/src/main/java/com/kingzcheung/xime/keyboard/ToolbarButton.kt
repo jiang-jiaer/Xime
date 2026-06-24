a392abdafceb35f2521edbb8ea55c8a0f6761814
Kor1 <kingzcheung@gmail.com>
2026-06-24 21:58:16 +0800

    feat(ToolbarButton): 添加悬浮功能按钮
    - 引入 PictureInPicture 图标用于悬浮功能
    - 在 ToolbarButton 枚举中添加 FLOAT 类型
    - 将 FLOAT 按钮设置为使用 PictureInPicture 图标
    - 移除多余的 Computer 图标导入
    - 调整枚举项顺序以保持逻辑一致性

--- app/src/main/java/com/kingzcheung/xime/keyboard/ToolbarButton.kt
+++ app/src/main/java/com/kingzcheung/xime/keyboard/ToolbarButton.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.twotone.Paid
 import androidx.compose.material.icons.twotone.Quickreply
 import androidx.compose.material.icons.twotone.SelectAll
 import androidx.compose.material.icons.filled.Computer
+import androidx.compose.material.icons.filled.PictureInPicture
 import androidx.compose.material.icons.twotone.ContentPasteGo
 import androidx.compose.ui.graphics.vector.ImageVector
 
@@ -25,12 +26,12 @@ enum class ToolbarButton(
     SCHEMA("schema", "方案选择", Icons.TwoTone.KeyboardAlt),
     QUICK_PHRASE("quick_phrase", "快捷发送", Icons.TwoTone.Quickreply),
     SYMBOL("symbol", "符号", Icons.TwoTone.Paid),
+    FLOAT("float", "悬浮", Icons.Default.PictureInPicture),
     SELECT_ALL("select_all", "全选", Icons.TwoTone.SelectAll),
     COPY("copy", "复制", Icons.TwoTone.ContentCopy),
     PASTE("paste", "黏贴", Icons.TwoTone.ContentPaste),
     HOME("home", "段首", Icons.TwoTone.FirstPage),
-    END("end", "段尾", Icons.AutoMirrored.TwoTone.LastPage),
-    FLOAT("float", "悬浮", Icons.Default.Computer);
+    END("end", "段尾", Icons.AutoMirrored.TwoTone.LastPage);
 
     companion object {
         val DEFAULT_VISIBLE = emptySet<ToolbarButton>()
